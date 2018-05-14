5c3f5b0611ba78ca4f02dec82fda8fbf3825cadb
Marc Lombry <marc.lombry@gmail.com>
2018-05-14 14:19:32 +0200

    Add files via upload

--- app/database/database.php
+++ app/database/database.php
@@ -21,4 +21,39 @@ class Database extends PDO{
 		return $this->pdo->query("SELECT * FROM $this->table")->fetchAll(PDO::FETCH_OBJ);
 	}
 
+	public function insert($donne){
+		/*
+		$q = $this->pdo->prepare("INSERT INTO $this->table (wordsen, picture, wordsfr) VALUES (:wordsen, :picture, :wordsfr)");
+		$q->execute($donne);
+		*/
+		
+		$q = $this->pdo->prepare("INSERT INTO $this->table (name, color, photo,href,category) VALUES (:name, :color, :photo, :href, :category)");
+		$q->execute($donne);
+		
+	}
+
+	public function delete($id,$table){
+		$q = $this->pdo->query("DELETE FROM $table WHERE id = $id");
+		$q->execute();
+	}
+
+	public function update($id,$table,$donne){
+		
+		$q = $this->pdo->prepare("UPDATE $table SET wordsen =:wordsen,picture =:picture, wordsfr=:wordsfr WHERE id = $id ");
+		$q->execute($donne);
+		/*
+		$q = $this->pdo->prepare("UPDATE $table SET name =:name,color =:color, photo=:photo
+		href = :href, category = :category WHERE id = $id ");
+		$q->execute($donne);
+		*/
+
+	}
+
+	public function setTable($table){
+		$this->table = $table;
+	}
+
+	public function getTable(){
+		return $this->table;
+	}
 }
