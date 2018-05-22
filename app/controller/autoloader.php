5fd83d2e324e4a9d1764697da59836d80e48c418
spikevargas <marc.lombry@gmail.com>
2018-05-22 21:47:12 +0200

    i added a comment for autoloader class

--- app/controller/autoloader.php
+++ app/controller/autoloader.php
@@ -1,4 +1,19 @@
 <?php
 class Autoloader{
+	/**
+	*@param array $arr array at format ['','','']
+	*
+	*/
+	public static function Autoload($arr){
+		if(is_array($arr))
+		{
+			foreach($arr as $v)
+			{
+				require 'controller/'.$v.'.php';
+			}
+		}else{
+			require 'controller/'.$arr.'.php';
+		}
+	}
 	
 }
